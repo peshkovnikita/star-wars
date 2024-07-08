fd174e8652723efe928eb85bb191aa6e9570c798
Nik <mrpepsi98@gmail.com>
2024-07-08 16:32:54 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import App from './components/app'
+
+import './bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//const swapi = new SwapiService();
+
+// swapi.getAllPerson()
+//     .then(people => {
+//         people.forEach(p => console.log(p.name))
+//     })
+//     .catch(err => console.log(err.message))
+
+// swapi.getAllStarships()
+//     .then(planets => {
+//         planets.forEach(p => console.log(p.name))
+//     })
+//     .catch(err => console.log(err.message))
+
+// swapi.getPerson(2)
+//     .then(person => console.log(person.name))
+//     .catch(err => console.log(err.message))
+
